6ffaaa9cfc80e59bd11ce927fb2581eb93aa11a3
Raja Manish <raghupatiraghav882@gmail.com>
2025-09-07 12:20:49 +0530

    Hangman game for guessing word with fun

---
+++ Hangman/main.py
@@ -0,0 +1,56 @@
+import random
+# import hangman_words
+# import hangman_art
+from hangman_words import word_list
+from hangman_art import stages,logo
+
+print(logo)
+
+lives = 6
+
+chosen_word = random.choice(word_list)
+print(chosen_word)
+
+placeholder = ""
+word_length = len(chosen_word)
+for position in range(word_length):
+    placeholder += "_"
+print("Word to guess: " + placeholder)
+
+game_over = False
+correct_letters = []
+
+while not game_over:
+    print(f"****************************{lives}/6 LIVES LEFT****************************")
+    guess = input("Guess a letter: ").lower()
+
+    if guess in correct_letters:
+        print(f"You've already guessed: {guess}")
+
+    display = ""
+
+    for letter in chosen_word:
+        if letter == guess:
+            display += letter
+            correct_letters.append(guess)
+        elif letter in correct_letters:
+            display += letter
+        else:
+            display += "_"
+
+    print("Word to guess: " + display)
+
+    if guess not in chosen_word:
+        lives -= 1
+        print(f"You have entered : {guess} is not in word. You lose a life.")
+        if lives == 0:
+            game_over = True
+
+            print(f"The correct word is : {chosen_word}")
+            print(f"***********************YOU LOSE**********************")
+
+    if "_" not in display:
+        game_over = True
+        print("****************************YOU WIN****************************")
+
+    print(stages[lives])
