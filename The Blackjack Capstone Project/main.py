2d9271ae88a24d171627e01ea3eae1cc58c45790
Raja Manish <raghupatiraghav882@gmail.com>
2025-09-12 10:44:49 +0530

    Black jack game 1st go

--- The Blackjack Capstone Project/main.py
+++ The Blackjack Capstone Project/main.py
@@ -7,22 +7,58 @@ cards = [11,2,3,4,5,6,7,8,9,10,10,10]
 should_continue = True
 
 def check():
-    check = print("Do you want to play a game of Blackjack? Type 'y' or 'n': ")
+    global should_continue
+    check = input("Do you want to play a game of Blackjack? Type 'y' or 'n': ")
     if check == "y":
         should_continue = True
     else:
+        print("Good Bye!!!")
         should_continue = False
 
-def sum(list):
+def sum(li):
     add = 0
-    for i in list:
+    for i in li:
         add +=i
 
     return add
 
-# print(f"Hello {sum(cards)}")
+def finalcheck(val1,val2):
+    if val1 >=21 or val2 >=21:
+        if val1 > val2:
+            print("You Win")
+        else:
+            print("Computer Win")
+        check()
 
-while should_continue:
+def drawChecker(com,my):
+    if com == my:
+       return True
+    else:
+        return False
+
+def checker():
+    checkerVal = input("Type 'y' to get another card, type 'n' to pass: ")
+    if checkerVal == "y":
+        return True
+    else:
+        return False
+    
+def computerSumCheckFor21():
+    global computersHand
+    global computersum
+    if computersum < 21:
+                 computersHand +=random.choices(cards,k=1)
+                 computersum = sum(computersHand)
+                 if computersum >= 21:
+                      finalcheck(computersum,mysum)
+                 else:
+                      computerSumCheckFor21()
+
+def firstGo():
+    global computersum
+    global mysum
+    global computersHand
+    global myHand
     computersHand = random.choices(cards,k=2)
     myHand = random.choices(cards,k=2)
     computersum = sum(computersHand)
@@ -33,16 +69,32 @@ while should_continue:
     print(f"Current Score :  {mysum}")
     print(f"Computer Score :  {computersum}")
 
-    checker = input("Type 'y' to get another card, type 'n' to pass: ")
 
-    if checker == "y":
-        if computersum == mysum:
-            ch
-            cs
+while should_continue:
+    firstGo()
+    if drawChecker(computersum,mysum) == True:
+            print("Draw")
+            check()
+            firstGo()
+    
+    if checker():
+        computersHand +=random.choices(cards,k=1)
+        myHand +=random.choices(cards,k=1)
+        print(f"Computer Hand: {computersHand}")
+        print(f"My Hand: {myHand}")
+        cs2 = sum(computersHand)
+        mys = sum(myHand)
+        print(f"Computer Sum: {cs2}")
+        print(f"My Sum: {mys}")
+        if drawChecker(cs2,mys) == True:
             print("Draw")
             check()
         else:
-            should_continue = True
+            finalcheck(mys,cs2)
+
     else:
-        should_continue = False
+        if mysum >= 21:
+             print("You Won")
+        else:
+            computerSumCheckFor21()
         
