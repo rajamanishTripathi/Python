eae5511551f8f649ec43d69b3c3c5a187b37ec00
Raja Manish <raghupatiraghav882@gmail.com>
2025-09-13 16:50:05 +0530

    Coffee machine project

--- Coffee Machine Project/main.py
+++ Coffee Machine Project/main.py
@@ -4,7 +4,7 @@ MENU = {
             "water": 50,
             "coffee": 18,
         },
-        "cost": 1.5,
+        "cost": 100,
     },
     "latte": {
         "ingredients": {
@@ -12,7 +12,7 @@ MENU = {
             "milk": 150,
             "coffee": 24,
         },
-        "cost": 2.5,
+        "cost": 200,
     },
     "cappuccino": {
         "ingredients": {
@@ -20,7 +20,7 @@ MENU = {
             "milk": 100,
             "coffee": 24,
         },
-        "cost": 3.0,
+        "cost": 300,
     }
 }
 
@@ -30,3 +30,51 @@ resources = {
     "coffee": 100,
 }
 
+should_continue = True
+
+def is_resource_sufficient(order_ingredients, resources):
+    """Returns True if ingredients are available, False if not."""
+    for item in order_ingredients:
+        if order_ingredients[item] > resources.get(item, 0):
+            print(f"Sorry, not enough {item}.")
+            return False
+    return True  #runs  after the loop — so it runs only if all items were checked and none failed.
+
+def ww(start):
+    if is_resource_sufficient(MENU[start]["ingredients"], resources):
+        print(f"Yes, enough resources to make a {start}!")
+        print("Please insert coins:")
+        cost = int(input("How many rupees? "))
+        # print(MENU[start]["cost"])
+        # print(cost)
+        # print(resources)
+        if cost == MENU[start]["cost"]:
+            print(f"You have paid {cost} successfully.")
+            for item in MENU[start]["ingredients"]:
+                resources[item] -= MENU[start]["ingredients"][item]
+            #print(resources)
+        else:
+            print("Please Pay the price")
+    else:
+        print(f"Cannot make {start}, insufficient resources.")
+        
+
+while should_continue:
+    start = input("What would you like? (espresso/latte/cappuccino):  ")
+
+    match start:
+        case "report":
+            for i,v in resources.items():
+               print(i+": ",v)
+
+        case "espresso":
+            ww(start)
+
+        case "latte":
+            ww(start)
+
+        case "cappuccino":
+            ww(start)
+            
+        case _:   # wildcard = "default"
+            print("Sorry, unknown choice")
